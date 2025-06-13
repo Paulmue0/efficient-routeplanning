d609348340f4d506fb5b70cd0abe9cc5ee4e61e6
PaulMue0 <muellerpaul2000@live.de>
2025-06-13 15:24:35 +0200

    chore: 🔧 Example usage of loading a network from data dir into a RoadNetwork struct

--- main.go
+++ main.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	parser "github.com/PaulMue0/efficient-routeplanning/Parser"
+)
+
 func main() {
+	name := "osm1.txt"
+	dataDir := "./data/RoadNetworks"
+	fileSystem := os.DirFS(dataDir)
+	network, err := parser.NewNetworkFromFS(fileSystem, name)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(network.NumNodes, network.NumEdges, len(network.Network.Vertices), len(network.Network.Edges))
 }
