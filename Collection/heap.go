8d68cda8ddc5599af8ccdfab81f64ced848bf7d8
PaulMue0 <muellerpaul2000@live.de>
2025-06-18 15:17:21 +0200

    feat: ✨ min heap caches Items for cleaner lookup

--- Collection/heap.go
+++ Collection/heap.go
@@ -9,46 +9,73 @@ import (
 
 // An Item is something we manage in a priority queue.
 type Item[T comparable] struct {
-	value    T
+	Value    T
 	priority float64
 	index    int
 }
 
-// Implements heap.Interface and holds Items.
-type PriorityQueue[T comparable] []*Item[T]
-
-func (pq PriorityQueue[T]) Len() int { return len(pq) }
-
-func (pq PriorityQueue[T]) Less(i, j int) bool {
-	// As this is a min heap we want Pop to give us the highest, not lowest, priority so we use greater than here.
-	return pq[i].priority > pq[j].priority
+func NewItem[T comparable](value T, prio float64, index int) *Item[T] {
+	return &Item[T]{value, prio, index}
 }
 
-func (pq PriorityQueue[T]) Swap(i, j int) {
-	pq[i], pq[j] = pq[j], pq[i]
-	pq[i].index = i
-	pq[j].index = j
+// Implements heap.Interface and holds Items.
+type PriorityQueue[T comparable] struct {
+	items []*Item[T]
+	index map[T]*Item[T]
+}
+
+func NewPriorityQueue[T comparable]() *PriorityQueue[T] {
+	return &PriorityQueue[T]{
+		items: []*Item[T]{},
+		index: make(map[T]*Item[T]),
+	}
+}
+
+func (pq *PriorityQueue[T]) Len() int {
+	return len(pq.items)
+}
+
+func (pq *PriorityQueue[T]) Less(i, j int) bool {
+	return pq.items[i].priority > pq.items[j].priority
+}
+
+func (pq *PriorityQueue[T]) Swap(i, j int) {
+	pq.items[i], pq.items[j] = pq.items[j], pq.items[i]
+	pq.items[i].index = i
+	pq.items[j].index = j
 }
 
 func (pq *PriorityQueue[T]) Push(x any) {
-	n := len(*pq)
 	item := x.(*Item[T])
-	item.index = n
-	*pq = append(*pq, item)
+	item.index = len(pq.items)
+	pq.items = append(pq.items, item)
+	pq.index[item.Value] = item
+}
+
+func (pq *PriorityQueue[T]) PushWithPriority(value T, priority float64) {
+	item := &Item[T]{Value: value, priority: priority}
+	heap.Push(pq, item)
 }
 
 func (pq *PriorityQueue[T]) Pop() any {
-	old := *pq
-	n := len(old)
-	item := old[n-1]
-	old[n-1] = nil  // don't stop the GC from reclaiming the item eventually
-	item.index = -1 // for safety
-	*pq = old[0 : n-1]
+	n := len(pq.items)
+	item := pq.items[n-1]
+	pq.items[n-1] = nil
+	pq.items = pq.items[:n-1]
+	item.index = -1
+	delete(pq.index, item.Value)
 	return item
 }
 
-func (pq *PriorityQueue[T]) update(item *Item[T], value T, priority float64) {
-	item.value = value
+func (pq *PriorityQueue[T]) Update(item *Item[T], value T, priority float64) {
+	item.Value = value
 	item.priority = priority
 	heap.Fix(pq, item.index)
+}
+
+func (pq *PriorityQueue[T]) UpdatePriority(value T, priority float64) {
+	if item, ok := pq.index[value]; ok {
+		item.priority = priority
+		heap.Fix(pq, item.index)
+	}
 }
